21153a13c291016bbe33d5fac0f60d68de968682
nikolakojadinovic <kojadinovic.nikola2205@gmail.com>
2024-04-27 11:15:07 +0200

    extract implemented

--- src/etl/server.py
+++ src/etl/server.py
@@ -6,14 +6,10 @@ from datetime import datetime, timedelta
 import os 
 from io import StringIO
 
-
-
 datasets = {
     "products":"https://temus-northstar.github.io/data_engineering_case_study_public/product_data.html",
     "vendors" :"https://temus-northstar.github.io/data_engineering_case_study_public/vendor_data.html"
 }
-
-LAST_MODIFIED = "" 
 
 def write_to_raw_storage(data: pd.DataFrame, dataset: str):
 
@@ -35,21 +31,32 @@ def write_to_raw_storage(data: pd.DataFrame, dataset: str):
 
 def load_data(dataset):
 
-    url = datasets[dataset]
+    LAST_MODIFIED = None
 
-    def get_last_modified():
-        return requests.get(url).headers.get("Last-Modified")
-    
-    global LAST_MODIFIED
-    LAST_MODIFIED = get_last_modified()
+    if not os.listdir(f"raw/{dataset}").__contains__("_LAST_MODIFIED"):
+        with open(f"raw/{dataset}/_LAST_MODIFIED", "w+") as f: 
+            f.write("Thu, 01 Jan 1970 00:00:00 GMT")
+
+    with open(f"raw/{dataset}/_LAST_MODIFIED", "r") as f:
+        LAST_MODIFIED = f.read()
+
+    url = datasets[dataset]
     
     response = requests.get(url,headers={"If-Modified-Since":LAST_MODIFIED})
+
+    LAST_MODIFIED = response.headers.get("Last-Modified")
+    if LAST_MODIFIED is not None:
+        with open(f"raw/{dataset}/_LAST_MODIFIED", "w+") as f: 
+            f.write(LAST_MODIFIED)
+
     data = pd.read_html(url)    
-    # print(response.text)
-    write_to_raw_storage(data[0], dataset)   
+    print(response.status_code)
+    if response.status_code == 304:
+        return 
+    if response.status_code == 200:
+        write_to_raw_storage(data[0], dataset)   
         
 
 load_data("products")
 load_data("vendors")
-
 
